192cd639ecebe1b42e877670f9142195431b4351
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-05-29 02:08:02 -0300

    feat(chapter 11): Implements pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,13 +3,16 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
+import { useDebouncedCallback } from "use-debounce";
+
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1"); // Reset to the first page on new search
 
     if (term) {
       params.set("query", term);
@@ -18,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
 
   return (
     <div className='relative flex flex-1 flex-shrink-0'>
